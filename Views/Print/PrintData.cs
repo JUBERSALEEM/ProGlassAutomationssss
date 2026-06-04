eaa6a170a56b07983e56ce4e737c9f254d52a488
JUBERSALEEM <techwithyassh@gmail.com>
2026-06-04 17:47:31 +0400

    DYNAMIC PRINT FOR JOBORDER: dnr

--- Views/Print/PrintData.cs
+++ Views/Print/PrintData.cs
@@ -1,50 +1,48 @@
-﻿using System;
-
-namespace ProGlassAutomation.Views.Print
+﻿namespace ProGlassAutomation.Views.Print
 {
     public class PrintData
     {
-        public string DocumentType { get; set; } = "Job Order";
-        public string DocumentNumber { get; set; } = "";
-        public DateTime DocumentDate { get; set; } = DateTime.Now;
-        public DateTime? ValidUntil { get; set; }
+        public string DocumentType { get; set; }
+        public string DocumentNumber { get; set; }
+        public DateTime DocumentDate { get; set; }
 
-        public string CompanyName { get; set; } = "";
-        public string CompanyTRN { get; set; } = "";
-        public string CompanyLocation { get; set; } = "";
+        // Company
+        public string CompanyName { get; set; }
+        public string CompanyTRN { get; set; }
+        public string CompanyLocation { get; set; }
 
-        public string CustomerName { get; set; } = "";
-        public string CustomerTRN { get; set; } = "";
-        public string CustomerAddress { get; set; } = "";
-        public string CustomerReference { get; set; } = "";
-        public string Salesman { get; set; } = "";
+        // Customer
+        public string CustomerName { get; set; }
+        public string CustomerTRN { get; set; }
+        public string CustomerAddress { get; set; }
+        public string CustomerReference { get; set; }
 
-        public string ProjectName { get; set; } = "";
-        public string ProjectNo { get; set; } = "";
-        public string ProjectLocation { get; set; } = "";
+        // Project
+        public string ProjectName { get; set; }
+        public string ProjectNo { get; set; }
+        public string ProjectLocation { get; set; }
 
-        public string LPONo { get; set; } = "";
-        public string AttentionName { get; set; } = "";
-        public string ContactNo { get; set; } = "";
+        // Additional
+        public string LPONo { get; set; }
+        public string AttentionName { get; set; }
+        public string ContactNo { get; set; }
+        public string Salesman { get; set; }
+        public string ReferencePI { get; set; }
+        public string Status { get; set; }
 
-        public string ReferencePI { get; set; } = "";
-        public string Status { get; set; } = "Pending";
-
+        // Totals
         public int TotalQty { get; set; }
         public double TotalSQM { get; set; }
         public double TotalLM { get; set; }
-        public double TotalSQM2 { get; set; }
-        public double TotalLM2 { get; set; }
-        public double TotalAmount { get; set; }
-        public string Notes { get; set; } = "";
 
-        public PrintItemData[] Items { get; set; } = Array.Empty<PrintItemData>();
+        public string Notes { get; set; }
+        public PrintItemData[] Items { get; set; }
     }
 
     public class PrintItemData
     {
         public int SrNo { get; set; }
-        public string GlassRef { get; set; } = "";
+        public string GlassRef { get; set; }
         public double Width1 { get; set; }
         public double Height1 { get; set; }
         public double Width2 { get; set; }
---
+++ Views/Print/PrintSettings.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ProGlassAutomation.Views.Print
+{
+    public class PrintSettings : INotifyPropertyChanged
+    {
+        private string _paperSize = "A4";
+        private bool _isLandscape = true;
+        private double _topMargin = 0.5;
+        private double _bottomMargin = 0.5;
+        private double _leftMargin = 0.5;
+        private double _rightMargin = 0.5;
+        private double _scale = 100;
+        private bool _fitToPage = true;
+        private bool _printGridLines = false;
+        private bool _printHeadings = true;
+        private bool _printNotes = true;
+        private double _zoom = 100;
+
+        public string PaperSize
+        {
+            get => _paperSize;
+            set { _paperSize = value; OnPropertyChanged(); ApplySettings(); }
+        }
+
+        public bool IsLandscape
+        {
+            get => _isLandscape;
+            set { _isLandscape = value; OnPropertyChanged(); OnPropertyChanged(nameof(OrientationDisplay)); ApplySettings(); }
+        }
+
+        public double TopMargin
+        {
+            get => _topMargin;
+            set { _topMargin = value; OnPropertyChanged(); ApplySettings(); }
+        }
+
+        public double BottomMargin
+        {
+            get => _bottomMargin;
+            set { _bottomMargin = value; OnPropertyChanged(); ApplySettings(); }
+        }
+
+        public double LeftMargin
+        {
+            get => _leftMargin;
+            set { _leftMargin = value; OnPropertyChanged(); ApplySettings(); }
+        }
+
+        public double RightMargin
+        {
+            get => _rightMargin;
+            set { _rightMargin = value; OnPropertyChanged(); ApplySettings(); }
+        }
+
+        public double Scale
+        {
+            get => _scale;
+            set { _scale = value; OnPropertyChanged(); ApplySettings(); }
+        }
+
+        public bool FitToPage
+        {
+            get => _fitToPage;
+            set { _fitToPage = value; OnPropertyChanged(); ApplySettings(); }
+        }
+
+        public bool PrintGridLines
+        {
+            get => _printGridLines;
+            set { _printGridLines = value; OnPropertyChanged(); }
+        }
+
+        public bool PrintHeadings
+        {
+            get => _printHeadings;
+            set { _printHeadings = value; OnPropertyChanged(); }
+        }
+
+        public bool PrintNotes
+        {
+            get => _printNotes;
+            set { _printNotes = value; OnPropertyChanged(); }
+        }
+
+        public double Zoom
+        {
+            get => _zoom;
+            set { _zoom = value; OnPropertyChanged(); ApplySettings(); }
+        }
+
+        public string OrientationDisplay => IsLandscape ? "Landscape" : "Portrait";
+
+        public string[] AvailablePaperSizes => new[] { "A4", "A3", "Letter", "Legal", "Tabloid" };
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler SettingsChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        private void ApplySettings()
+            => SettingsChanged?.Invoke(this, EventArgs.Empty);
+
+        public void ToggleOrientation()
+        {
+            IsLandscape = !IsLandscape;
+        }
+
+        public void SetScaleFromIndex(int index)
+        {
+            Scale = index switch
+            {
+                0 => 50,
+                1 => 75,
+                2 => 100,
+                3 => 0, // Fit to page
+                _ => 100
+            };
+            FitToPage = index == 3;
+        }
+
+        public void SetPaperSizeFromIndex(int index)
+        {
+            if (index >= 0 && index < AvailablePaperSizes.Length)
+                PaperSize = AvailablePaperSizes[index];
+        }
+
+        public void SetMargin(string margin, double value)
+        {
+            switch (margin.ToUpper())
+            {
+                case "T": TopMargin = value; break;
+                case "B": BottomMargin = value; break;
+                case "L": LeftMargin = value; break;
+                case "R": RightMargin = value; break;
+            }
+        }
+    }
+}
